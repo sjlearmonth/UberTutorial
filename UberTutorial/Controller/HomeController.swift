b859b664baf1eef79ce20558adeb34888b697bee
Stephen J Learmonth <stephen.j.learmonth@gmail.com>
2020-05-01 18:11:38 +0100

    user's location added

--- UberTutorial/Controller/HomeController.swift
+++ UberTutorial/Controller/HomeController.swift
@@ -14,12 +14,15 @@ class HomeController: UIViewController {
     
     // MARK: - Properties
     private let mapView = MKMapView()
+    private let locationManager = CLLocationManager()
+    private let inputActivationView = LocationInputActivationView()
     
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         checkIfUserIsLoggedIn()
+        enableLocationServices()
 //        signOut()
     }
     
@@ -45,8 +48,61 @@ class HomeController: UIViewController {
         }
     }
     
+    // MARK: - Helper Functions
+    
     func configureUI() {
+        configureMapView()
+        
+        view.addSubview(inputActivationView)
+        inputActivationView.centerX(inView: view)
+        inputActivationView.setDimensions(height: 50, width: view.frame.width - 64)
+        inputActivationView.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+    }
+    
+    func configureMapView() {
         view.addSubview(mapView)
         mapView.frame = view.frame
+        
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+
     }
+}
+
+// MARK: - Location Services
+
+extension HomeController: CLLocationManagerDelegate {
+    
+    func enableLocationServices() {
+        locationManager.delegate = self
+        switch CLLocationManager.authorizationStatus() {
+            
+        case .notDetermined:
+            print("DEBUG: Not determined.")
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            break
+        case .denied:
+            break
+        case .authorizedAlways:
+            print("DEBUG: Auth always.")
+            locationManager.startUpdatingLocation()
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        case .authorizedWhenInUse:
+            print("DEBUG: Auth when in use.")
+            locationManager.requestAlwaysAuthorization()
+        @unknown default:
+            print("DEBUG: default")
+            break
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        if status == .authorizedWhenInUse {
+            locationManager.requestAlwaysAuthorization()
+        }
+    }
+    
 }
---
+++ UberTutorial/View/LocationInputActivationView.swift
@@ -0,0 +1,47 @@
+//
+//  LocationInputActivationView.swift
+//  UberTutorial
+//
+//  Created by Stephen Learmonth on 01/05/2020.
+//  Copyright © 2020 Stephen Learmonth. All rights reserved.
+//
+
+import UIKit
+
+class LocationInputActivationView : UIView {
+    
+    // MARK: - Properties
+    
+    private let indicatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        return view
+    }()
+    
+    private let placeholderLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Where to?"
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textColor = .darkGray
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .white
+        
+        addSubview(indicatorView)
+        indicatorView.centerY(inView: self, leftAnchor: leftAnchor, paddingLeft: 16)
+        indicatorView.setDimensions(height: 6, width: 6)
+        
+        addSubview(placeholderLabel)
+        placeholderLabel.centerY(inView: self, leftAnchor: indicatorView.rightAnchor, paddingLeft: 20)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
